575968156b50188a88c4484f88be7b85ac800d00
kevchcm <kevin@maniak.com.mx>
2023-07-13 13:19:21 -0600

    added pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,9 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())
             <x-posts-grid :posts="$posts"/>
+            
+            {{--$items->links('pagination::bootstrap-4')--}}
+            {{ $posts->links('pagination::tailwind') }}
         @else
             <p>
                 No posts yet. Please check back later.
